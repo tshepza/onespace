9aa3f916d755a06f869ceb016a9cab099fef3379
Tshepza <shamaneta@gmail.com>
2024-01-22 08:40:14 +0200

    fixing test to execute the valid input first

--- conversion-lib/src/test/java/com/onespace/conversion/MessageServiceProcessorTest.java
+++ conversion-lib/src/test/java/com/onespace/conversion/MessageServiceProcessorTest.java
@@ -7,16 +7,18 @@ import org.junit.Test;
 public class MessageServiceProcessorTest {
 
 
-    @Test
-    public void testWithInValidInput() {
-        System.out.println("======================INVALID INPUT TEST====================");
-        //new MessageServiceProcessor().process(TestInput.TEST_INVALID_INPUT);
-    }
 
     @Test
     public void testWithValidInput() {
         System.out.println("======================VALID INPUT TEST====================");
         new MessageServiceProcessor().process(TestInput.TEST_INPUT);
     }
+
+    @Test
+    public void testWithInValidInput() {
+        System.out.println("======================INVALID INPUT TEST====================");
+        new MessageServiceProcessor().process(TestInput.TEST_INVALID_INPUT);
+    }
+
 
 }
